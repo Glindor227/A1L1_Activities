f217fd37bf38d281079fe2fabb07073dfd567dfe
Glindor227 <glindor227@gmail.com>
2019-04-23 15:02:12 +0300

    ПЗ к уроку 2

--- app/src/main/java/com/geekbrains/a1l1_activities/MainActivity.java
+++ app/src/main/java/com/geekbrains/a1l1_activities/MainActivity.java
@@ -9,13 +9,26 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "GlindorLog";
     TextView counterTextView;
+
+    private void viewCycleInfo(String message){
+        Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT).show();
+        Log.d(TAG, message);
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toast.makeText(getApplicationContext(), "onCreate", Toast.LENGTH_SHORT).show();
+        String instanceState;
+        if (savedInstanceState == null)
+            instanceState = "Первый запуск ";
+        else
+            instanceState = "Повторный запуск ";
+
+        viewCycleInfo(instanceState + "onCreate");
 
         counterTextView = findViewById(R.id.counterText);
 
@@ -39,30 +52,30 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onStart() {
         super.onStart();
-        Toast.makeText(getApplicationContext(), "onStart", Toast.LENGTH_SHORT).show();
+        viewCycleInfo("onStart");
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        Toast.makeText(getApplicationContext(), "onResume", Toast.LENGTH_SHORT).show();
+        viewCycleInfo("onResume");
     }
 
     @Override
     protected void onPause() {
         super.onPause();
-        Toast.makeText(getApplicationContext(), "onPause", Toast.LENGTH_SHORT).show();
+        viewCycleInfo("onPause");
     }
 
     @Override
     protected void onRestart() {
         super.onRestart();
-        Toast.makeText(getApplicationContext(), "onRestart", Toast.LENGTH_SHORT).show();
+        viewCycleInfo("onRestart");
     }
 
     @Override
     protected void onStop() {
         super.onStop();
-        Toast.makeText(getApplicationContext(), "onStop", Toast.LENGTH_SHORT).show();
+        viewCycleInfo("onStop");
     }
 }
